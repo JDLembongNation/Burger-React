fa1c9004a188cc5365bbe2e31aecae7c07239146
Joshua Domenico Lembong <lembongjoshua@gmail.com>
2020-07-25 14:38:06 +0100

    Added Toggle Sidebar

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -14,11 +14,18 @@ class Layout extends Component{
         this.setState({showSideDrawer: false});
     }
 
+    sideDrawerToggleHandler = () => {
+        //When adjusting new states based on previous state, 
+        //Use the prev state parameter as setstate is an asynchronous function
+        this.setState((prevState) => {
+            return {showSideDrawer: !prevState.showSideDrawer};
+        });
+    }
 
     render(){ 
         return(
             <Auxillary>
-                <Toolbar />
+                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
                 <SideDrawer closed={this.sideDrawerClosedHandler} open={this.state.showSideDrawer} />
                 <main className={classes.Content}>
                     {this.props.children}
